a9247c01b508b138dda6efb07d594930828efd64
Lovish Garg <lovishgarg@Lovishs-MacBook-Pro.local>
2022-03-25 14:57:08 +0530

    25-03-2022

--- server/manifest.js
+++ server/manifest.js
@@ -1,45 +1,45 @@
-'use strict';
+"use strict";
 
-const Dotenv = require('dotenv');
-const Confidence = require('@hapipal/confidence');
-const Toys = require('@hapipal/toys');
+const Dotenv = require("dotenv");
+const Confidence = require("@hapipal/confidence");
+const Toys = require("@hapipal/toys");
 
 // Pull .env into process.env
 Dotenv.config({ path: `${__dirname}/.env` });
 
 // Glue manifest as a confidence store
 module.exports = new Confidence.Store({
-    server: {
-        host: 'localhost',
-        port: {
-            $param: 'PORT',
-            $coerce: 'number',
-            $default: 3000
-        },
-        debug: {
-            $filter: 'NODE_ENV',
-            $default: {
-                log: ['error', 'start'],
-                request: ['error']
-            },
-            production: {
-                request: ['implementation']
-            }
-        }
+  server: {
+    host: "localhost",
+    port: {
+      $param: "PORT",
+      $coerce: "number",
+      $default: 3000,
     },
-    register: {
-        plugins: [
-            {
-                plugin: '../lib', // Main plugin
-                options: {}
-            },
-            {
-                plugin: {
-                    $filter: 'NODE_ENV',
-                    $default: '@hapipal/hpal-debug',
-                    production: Toys.noop
-                }
-            }
-        ]
-    }
+    debug: {
+      $filter: "NODE_ENV",
+      $default: {
+        log: ["error", "start"],
+        request: ["error"],
+      },
+      production: {
+        request: ["implementation"],
+      },
+    },
+  },
+  register: {
+    plugins: [
+      {
+        plugin: "../lib", // Main plugin
+        options: {},
+      },
+      {
+        plugin: {
+          $filter: "NODE_ENV",
+          $default: "@hapipal/hpal-debug",
+          production: Toys.noop,
+        },
+      },
+    ],
+  },
 });
